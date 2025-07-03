2c04dbe4039d0b1cfc72dfd4bd5133276621a70b
kernelfucker <dikodut@protonmail.com>
2025-07-03 12:26:34 -0400

    kill yourself

--- death.c
+++ death.c
@@ -6,13 +6,28 @@
 #include <unistd.h>
 #include <libgen.h>
 
-#define SECTOR_SIZE 512
-#define INTERVAL 1
+#define sector_size 512
+#define interval 1
+
+#define version "0.2"
 
 typedef struct{
 	unsigned long long read_bytes;
 	unsigned long long write_bytes;
 } IOStat;
+
+void help(const char *death){
+	printf("usage: %s [options]..\n", death);
+	printf("options:\n");
+	printf("  -h	display this\n");
+	printf("  -v	show version information\n");
+	exit(1);
+}
+
+void show_version(){
+	printf("death-%s\n", version);
+	exit(1);
+}
 
 void get_io(const char *devname, IOStat *stat){
 	FILE *fp = fopen("/proc/diskstats", "r");
@@ -26,10 +41,10 @@ void get_io(const char *devname, IOStat *stat){
 		char dev[32];
 		unsigned long rd_sec, wr_sec;
 
-		int matched = sscanf(line, "%*d %*d %31s %*u %*u %*u %*u %*u %*u %llu %llu", dev, &rd_sec, &wr_sec);
+		int matched = sscanf(line, "%*d %*d %31s %*u %*u %*u %*u %*u %*u %lu %lu", dev, &rd_sec, &wr_sec);
 		if(matched == 3 && strcmp(dev, devname) == 0){
-			stat->read_bytes = (unsigned long long)rd_sec * SECTOR_SIZE;
-			stat->write_bytes = (unsigned long long)wr_sec * SECTOR_SIZE;
+			stat->read_bytes = (unsigned long long)rd_sec * sector_size;
+			stat->write_bytes = (unsigned long long)wr_sec * sector_size;
 			fclose(fp);
 			return;
 		}
@@ -41,8 +56,19 @@ void get_io(const char *devname, IOStat *stat){
 }
 
 int main(int argc, char *argv[]){
+	if(argc == 2){
+		if(strcmp(argv[1], "-h") == 0){
+			help(argv[0]);
+		}
+
+		if(strcmp(argv[1], "-v") == 0){
+			show_version();
+		}
+	}
+
 	if(argc != 2){
 		fprintf(stderr, "usage: %s /dev/sdX\n", argv[0]);
+		fprintf(stderr, "try '%s -h' for more information\n", argv[0]);
 		exit(1);
 	}
 
@@ -53,7 +79,7 @@ int main(int argc, char *argv[]){
 	get_io(devname, &prev);
 
 	while(1){
-		sleep(INTERVAL);
+		sleep(interval);
 		get_io(devname, &curr);
 
 		unsigned long long r = curr.read_bytes - prev.read_bytes;
